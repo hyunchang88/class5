82078df455b600067dbadcf56ceb63b63206f365
hyunchang <hyunchang1234@hanmail.net>
2017-07-05 10:56:36 +0900

    folder 정리

--- WebContent/WEB-INF/data_insert.sql
+++ WebContent/WEB-INF/data_insert.sql
@@ -1,53 +1,57 @@
 
-insert into job(jobNo, jobName, jobDesc)
-   values(jobNo_SEQ.NEXTVAL, '안방 인테리어', '아파트 안방을 인테리어 하는 작업입니다.');
-insert into job(jobNo, jobName, jobDesc)
-   values(jobNo_SEQ.NEXTVAL, '거실 인테리어', '아파트 거실을 인테리어 하는 작업입니다.');
-insert into job(jobNo, jobName, jobDesc)
-   values(jobNo_SEQ.NEXTVAL, '싱크대 인테리어', '아파트 싱크대를 인테리어 하는 작업입니다.');
-insert into job(jobNo, jobName, jobDesc)
-   values(jobNo_SEQ.NEXTVAL, '화장실 인테리어', '아파트 화장실을 인테리어 하는 작업입니다.');
-insert into job(jobNo, jobName, jobDesc)
-   values(jobNo_SEQ.NEXTVAL, '테라스 인테리어', '아파트 테라스를 인테리어 하는 작업입니다.');
+insert into work(workNo, workName, workDesc)
+   values(workNo_SEQ.NEXTVAL, '안방 인테리어', '아파트 안방을 인테리어 하는 작업입니다.');
+insert into work(workNo, workName, workDesc)
+   values(workNo_SEQ.NEXTVAL, '거실 인테리어', '아파트 거실을 인테리어 하는 작업입니다.');
+insert into work(workNo, workName, workDesc)
+   values(workNo_SEQ.NEXTVAL, '싱크대 인테리어', '아파트 싱크대를 인테리어 하는 작업입니다.');
+insert into work(workNo, workName, workDesc)
+   values(workNo_SEQ.NEXTVAL, '화장실 인테리어', '아파트 화장실을 인테리어 하는 작업입니다.');
+insert into work(workNo, workName, workDesc)
+   values(workNo_SEQ.NEXTVAL, '테라스 인테리어', '아파트 테라스를 인테리어 하는 작업입니다.');
    
-insert into company(companyNo, companyName, companyCeoName, companyCeoPhone, jobArea, jobNo)
+insert into company(companyNo, companyName, companyCeoName, companyCeoPhone, workArea, workNo)
    values(companyNo_SEQ.NEXTVAL, '(주)그림', '김씨랑', '010-2325-3389', '경기도', 1);
-insert into company(companyNo, companyName, companyCeoName, companyCeoPhone, jobArea, jobNo)
+insert into company(companyNo, companyName, companyCeoName, companyCeoPhone, workArea, workNo)
    values(companyNo_SEQ.NEXTVAL, '(주)인터', '박대성', '010-2325-3389', '경기도', 2);
-insert into company(companyNo, companyName, companyCeoName, companyCeoPhone, jobArea, jobNo)
+insert into company(companyNo, companyName, companyCeoName, companyCeoPhone, workArea, workNo)
    values(companyNo_SEQ.NEXTVAL, '(주)시그린', '이대로', '010-2325-3389', '서울', 3);
-insert into company(companyNo, companyName, companyCeoName, companyCeoPhone, jobArea, jobNo)
+insert into company(companyNo, companyName, companyCeoName, companyCeoPhone, workArea, workNo)
    values(companyNo_SEQ.NEXTVAL, '(주)아람들', '김사랑', '010-2325-3389', '서울', 4);
-insert into company(companyNo, companyName, companyCeoName, companyCeoPhone, jobArea, jobNo)
+insert into company(companyNo, companyName, companyCeoName, companyCeoPhone, workArea, workNo)
    values(companyNo_SEQ.NEXTVAL, '(주)멋쟁이', '안정해', '010-2325-3389', '제주도', 5);
    
 insert into member(memberId, memberPwd, memberName, 
-   companyNo, memberLevel, memberEmail, memberPhone, jobArea)
+   companyNo, memberLevel, memberEmail, memberPhone, workArea)
    values('user01', '0101', '유일위',
    1, '9', 'youilwi@naver.com', '010-6517-6369', '인천');
-insert into member(memberNo, memberId, memberPwd, memberName, 
-   companyNo, memberLevel, memberEmail, memberPhone, jobArea)
+insert into member(memberId, memberPwd, memberName, 
+   companyNo, memberLevel, memberEmail, memberPhone, workArea)
    values('user02', '0202', '박현창',
-   2, '9', 'park@naver.com', '010-6517-6369', '서울');
-insert into member(memberNo, memberId, memberPwd, memberName, 
-   companyNo, memberLevel, memberEmail, memberPhone, jobArea)
+   2, '9', 'hyunchang1234@hanmail.net', '010-4186-6104', '일산');
+insert into member(memberId, memberPwd, memberName, 
+   companyNo, memberLevel, memberEmail, memberPhone, workArea)
    values('user03', '0303', '문병우',
    3, '9', 'mun@naver.com', '010-6517-6369', '경기도');
-insert into member(memberNo, memberId, memberPwd, memberName, 
-   companyNo, memberLevel, memberEmail, memberPhone, jobArea)
+insert into member(memberId, memberPwd, memberName, 
+   companyNo, memberLevel, memberEmail, memberPhone, workArea)
    values('user04', '0404', '유일위',
    4, '9', 'sang@naver.com', '010-6517-6369', '제주');
-insert into member(memberNo, memberId, memberPwd, memberName, 
-   companyNo, memberLevel, memberEmail, memberPhone, jobArea)
+insert into member(memberId, memberPwd, memberName, 
+   companyNo, memberLevel, memberEmail, memberPhone, workArea)
    values('user05', '0505', '이순신',
    5, '1', 'kim@naver.com', '010-6517-6369', '전라도');
    
-insert into orderRequest(orderNo, memberId, jobNo, companyNo, orderTitle,
+insert into orderRequest(orderNo, memberId, workNo, companyNo, orderTitle,
    orderPrice, orderComm, orderStatus, orderResult, startDate, enddate)
    values(orderNo_SEQ.NEXTVAL, 'user01', 1, 1, '인테리어 첫공사', 1000000, 100000,
-   '1', 'B', SysDate,);
+   '1', 'B', SysDate, null);
    
 insert into board(boardNo, memberId, boardTitle, boardContent, regDate,
    readCount, boardType, filePath, PhotoPath)
    values(boardNo_SEQ.NEXTVAL, 'user01', '견적요청서 공지사할 입니다.', '견적요청서를 작성할 때 주의사항입니다.',
    SysDate, 0, 'A', 'C:\class5\file\', 'C:\class5\photo\');
+   
+   
+   
+   
--- WebContent/WEB-INF/table_creation.sql
+++ WebContent/WEB-INF/table_creation.sql
@@ -1,19 +1,21 @@
-CREATE TABLE job(
-   jobNo NUMBER(7) CONSTRAINT job_jobNo_pk PRIMARY KEY,
-   jobName VARCHAR2(50) NOT NULL,
-   jobDesc VARCHAR2(300)
-)
-CREATE SEQUENCE jobNo_SEQ
+CREATE TABLE work(
+   workNo NUMBER(7) CONSTRAINT work_workNo_pk PRIMARY KEY,
+   workName VARCHAR2(50) NOT NULL,
+   workDesc VARCHAR2(300)
+);
+
+CREATE SEQUENCE workNo_SEQ;
 
 CREATE TABLE company(
    companyNo NUMBER(7) CONSTRAINT company_companyNo_pk PRIMARY KEY,
    companyName VARCHAR2(50) NOT NULL,
    companyCeoName VARCHAR2(50) NOT NULL,
    companyCeoPhone VARCHAR2(20) NOT NULL,
-   jobArea VARCHAR2(20) NOT NULL,
-   jobNo NUMBER(7) CONSTRAINT company_jobNo_fk REFERENCES job(jobNo)
-)
-CREATE SEQUENCE companyNo_SEQ
+   workArea VARCHAR2(20) NOT NULL,
+   workNo NUMBER(7) CONSTRAINT company_workNo_fk REFERENCES work(workNo)
+);
+
+CREATE SEQUENCE companyNo_SEQ;
 
 CREATE TABLE member(
    memberId VARCHAR2(50) CONSTRAINT member_memberId_pk PRIMARY KEY,
@@ -23,13 +25,13 @@ CREATE TABLE member(
    memberLevel CHAR(1) NOT NULL,
    memberEmail VARCHAR2(50) NOT NULL,
    memberPhone VARCHAR2(20) NOT NULL,
-   jobArea VARCHAR2(20) NOT NULL
-)
+   workArea VARCHAR2(20) NOT NULL
+);
 
 CREATE TABLE orderRequest(
    orderNo NUMBER(7) CONSTRAINT orderRequest_orderNo_pk PRIMARY KEY,
    memberId VARCHAR2(50) CONSTRAINT orderRequest_memberId_fk REFERENCES member(memberId),
-   jobNo NUMBER(7) CONSTRAINT orderRequest_jobNo_fk REFERENCES job(jobNo),
+   workNo NUMBER(7) CONSTRAINT orderRequest_workNo_fk REFERENCES work(workNo),
    companyNo NUMBER(7) CONSTRAINT orderRequest_companyNo_fk REFERENCES company(companyNo),
    orderTitle VARCHAR2(50) NOT NULL,
    orderPrice NUMBER(9) default 0,
@@ -38,8 +40,9 @@ CREATE TABLE orderRequest(
    orderResult CHAR(1),
    startDate DATE NOT NULL,
    enddate DATE
-)
-CREATE SEQUENCE orderNo_SEQ
+);
+
+CREATE SEQUENCE orderNo_SEQ;
 
 CREATE TABLE board(
    boardNo NUMBER(7) CONSTRAINT board_boardNo_pk PRIMARY KEY,
@@ -51,5 +54,7 @@ CREATE TABLE board(
    boardType CHAR(1) NOT NULL,
    filePath VARCHAR2(100),
    PhotoPath VARCHAR2(100)
-)
+);
+
 CREATE SEQUENCE boardNo_SEQ;
+
